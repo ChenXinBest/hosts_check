b15820add14c40d4e3b851d57d96b8f6b0a337c6
BigDaddy <62179299+ChenXinBest@users.noreply.github.com>
2025-09-24 10:18:48 +0800

    Update DailyJob.py
    添加github域名解析

--- DailyJob.py
+++ DailyJob.py
@@ -9,7 +9,7 @@ from ping3 import ping
 api = "http://api.ip33.com/dns/resolver"
 
 # 待解析的域名
-hosts = ["api.themoviedb.org", "image.tmdb.org", "www.themoviedb.org"]
+hosts = ["api.themoviedb.org", "image.tmdb.org", "www.themoviedb.org","alive.github.com","api.github.com","assets-cdn.github.com","avatars.githubusercontent.com","avatars0.githubusercontent.com","avatars1.githubusercontent.com","avatars2.githubusercontent.com","avatars3.githubusercontent.com","avatars4.githubusercontent.com","avatars5.githubusercontent.com","camo.githubusercontent.com","central.github.com","cloud.githubusercontent.com","codeload.github.com","collector.github.com","desktop.githubusercontent.com","favicons.githubusercontent.com","gist.github.com","github-cloud.s3.amazonaws.com","github-com.s3.amazonaws.com","github-production-release-asset-2e65be.s3.amazonaws.com","github-production-repository-file-5c1aeb.s3.amazonaws.com","github-production-user-asset-6210df.s3.amazonaws.com","github.blog","github.com","github.community","github.githubassets.com","github.global.ssl.fastly.net","github.io","github.map.fastly.net","githubstatus.com","live.github.com","media.githubusercontent.com","objects.githubusercontent.com","pipelines.actions.githubusercontent.com","raw.githubusercontent.com","user-images.githubusercontent.com","vscode.dev","education.github.com","private-user-images.githubusercontent.com"]
 
 # dns服务商
 dnsProvider = ["156.154.70.1", "208.67.222.222"]
